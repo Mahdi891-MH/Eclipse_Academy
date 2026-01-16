d3be34e03f1cc263dab8ff279eb8f8caf143e558
Mahdi891-MH <mahdihussaini891@gmail.com>
2026-01-16 18:01:24 +0430

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+document.getElementsByClassName("hide")[0].addEventListener("click", hide)
+counter = 0;
+function hide() {
+    if (counter % 2 == 0) {
+        document.getElementById("navbar").style.display = "block";
+        document.getElementsByClassName("hide")[0].setAttribute("src", "images/cross.png");
+    }
+
+    else {
+        document.getElementById("navbar").style.display = "none";
+        document.getElementsByClassName("hide")[0].setAttribute("src", "images/threeline.png");
+    }
+
+    counter++;
+
+}
